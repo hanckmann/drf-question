c07c4320140d93a3ef8bcc9340dd1d65b3def047
Patrick Hanckmann <hanckmann@gmail.com>
2021-01-17 17:57:09 +0100

    finished part 6

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -6,8 +6,7 @@ from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
-    highlight = serializers.HyperlinkedIdentityField(
-        view_name='snippet-highlight', format='html')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
 
     class Meta:
         model = Snippet
