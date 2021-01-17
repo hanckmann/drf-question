8dda313af3c5d29c6c6f5bab3f6d2ff6798abfdb
Patrick Hanckmann <hanckmann@gmail.com>
2021-01-17 14:55:21 +0100

    Here the reverse should work, but they do not

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,16 +1,26 @@
 # snippets/serializers
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
+
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
         model = Snippet
         fields = ('id', 'title', 'code', 'linenos',
-                  'language', 'style')
-                  # 'language', 'style', 'url')
+                  'language', 'style', 'owner',)
+                  # 'url')
 
         # extra_kwargs = {
         #     "url": {"view_name": "snippet-detail", "lookup_field": "pk"},
         # }
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'snippets')
--- snippets/views.py
+++ snippets/views.py
@@ -1,13 +1,53 @@
 # snippets/views.py
-from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework import generics, permissions # new
+from rest_framework.decorators import api_view # new
+from rest_framework.response import Response # new
+from rest_framework.reverse import reverse # new
+
 from .models import Snippet
-from .serializers import SnippetSerializer
+from .permissions import IsOwnerOrReadOnly # new
+from .serializers import SnippetSerializer, UserSerializer
+
+
+@api_view(['GET']) # new
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
+
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,) # new
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,) # new
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# snippets/views.py
+from django.contrib.auth.models import User
+from rest_framework import generics, permissions
+
+from .models import Snippet
+from .permissions import IsOwnerOrReadOnly
+from .serializers import SnippetSerializer, UserSerializer
